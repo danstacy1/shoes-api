8bb9974f564108cb3a2378c882027025c2ae1f89
danstacy1 <danstacy1@gmail.com>
2022-07-29 23:11:19 -0400

    build seed.js and shoe.js

---
+++ app/models/seed.js
@@ -0,0 +1,50 @@
+// seed.js is going to be the file we run, whenever we want to seed our database, we'll create a bunch of pets at once.
+
+// we want to be careful with this, because when we run it, it'll delete all of the shoes in the db. 
+
+// we can modify this later, to only delete shoes that don't have an owner already, but we'll keep it simple for now.
+
+const mongoose = require('mongoose')
+const Shoe = require('./shoe')
+const db = require('../../config/db')
+
+const startShoes = [
+    { brand: 'Nike', name: 'Jordan 3 Retro UNC', color: 'White/Grey', style: 'Basketball', forsale: false},
+    { brand: 'Nike', name: 'Jordan 11 Concord', color: 'White/Black', style: 'Basketball', forsale: true},
+    { brand: 'adidas', name: 'UltraBoost 4.0 DNA', color: 'White', style: 'Running', forsale: false},
+    { brand: 'adidas', name: 'Yeezy Boost 350 V2', color: 'White/Black/Red', style: 'Running', forsale: true},
+    { brand: 'Vans', name: 'Old Skool Classic', color: 'Black/White', style: 'Skating', forsale: true},
+    { brand: 'Crocs', name: 'Classic Clog', color: 'Tie-Dye', style: 'Leisure', forsale: true},
+    { brand: 'Chaco', name: 'Z Cloud ', color: 'Grey/Yellow/Red', style: 'Sandels', forsale: false}
+]
+
+// first we need to connect to the database
+mongoose.connect(db, {
+    useNewUrlParser: true
+})
+    .then(() => {
+        // first we remove all of the shoes
+        // here we can add something to make sure we only delete shoes without an owner
+        Shoe.deleteMany({ owner: null })
+            .then(deletedShoes => {
+                console.log('deletedShoes', deletedShoes)
+                // the next step is to use our startShoes array to create our seeded shoes
+                Shoe.create(startShoes)
+                    .then(newShoes => {
+                        console.log('the new shoes', newShoes)
+                        mongoose.connection.close()
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        mongoose.connection.close()
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+                mongoose.connection.close()
+            })
+    })
+    .catch(error => {
+        console.log(error)
+        mongoose.connection.close()
+    })
---
+++ app/models/shoe.js
@@ -0,0 +1,66 @@
+// Shoe -> have an owner field, that is a user
+// eventaully we'll add an array of toy subdocuments
+
+const mongoose = require('mongoose')
+
+// const shoeSchema = require('./shoe')
+
+const { Schema, model } = mongoose
+const shoeSchema = new Schema(
+    {
+        brand: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        color: {
+            type: String,
+            required: true
+        },
+        style: {
+            type: String,
+            required: true
+        },
+        forsale: {
+            type: Boolean,
+            required: true
+        },
+
+        // toys: [toySchema],
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }, {
+        timestamps: true,
+        // we're going to be adding virtuals to our model, the following lines will make sure that those virtuals are included whenever we return JSON or an Object
+        toObject: { virtuals: true },
+        toJSON: {virtuals: true }
+    }
+)
+
+// virtuals go here
+// these are virtual properties, that use existing data (saved in the database), to add a property whenever we retrieve a document and convert it to JSON or an object
+// use traditional function syntax
+shoeSchema.virtual('fullTitle').get(function () {
+    // in here, we can do whatever javascripty things we want, to make sure we return some value that will be assigned to this virtual
+    // fullTitle is going to combine the brand and name to build a title
+    return `${this.brand} the ${this.name}`
+})
+
+
+///////////NOT sure how to use this in my app for now///
+// shoeSchema.virtual('isABaby').get(function () {
+//     if (this.age < 2) {
+//         return "yeah, they're just a baby"
+//     } else if (this.age >= 2 && this.age < 6) {
+//         return "not really a baby, but still a baby"
+//     } else {
+//         return "a good old pet(definitely still a baby)"
+//     }
+// })
+
+module.exports = model('Shoe', shoeSchema)
